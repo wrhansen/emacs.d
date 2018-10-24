387e347926bd980ab4276809906a8e39c8213236
Wesley Hansen <wesrhansen@gmail.com>
2018-10-23 22:18:31 -0400

    Okay, I think I have finally worked out how to set the font
    I will see for sure when I pull it down at work.

--- init.el
+++ init.el
@@ -158,12 +158,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
  '(package-selected-packages
    (quote
-    (git-gutter solarized-theme projectile org-bullets neotree markdown-mode magit dashboard all-the-icons))))
+    (git-gutter solarized-theme projectile org-bullets neotree markdown-mode magit dashboard all-the-icons)))
+ '(show-paren-mode t)
+ '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "Iosevka" :foundry "CYEL" :slant normal :weight normal :height 113 :width normal)))))
